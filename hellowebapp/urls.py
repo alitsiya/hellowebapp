dedd945af24622330e03cccda11467453cd12482
alitsiya <aliaj@MacBook-Pro.local>
2015-09-26 14:01:23 -0700

    Edit thing

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -10,5 +10,7 @@ urlpatterns = patterns('',
         TemplateView.as_view(template_name='contact.html'),
         name='contact'),
     url(r'^things/(?P<slug>[-\w]+)/$', 'collection.views.thing_detail', name='thing_detail'),
+    # new line we're adding!
+    url(r'^things/(?P<slug>[-\w]+)/edit/$', 'collection.views.edit_thing', name='edit_thing'),
     url(r'^admin/', include(admin.site.urls)),
 )
