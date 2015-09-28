1842a810367f20733d3be1b57a4aed234a200bf1
Alitsiya <aliaj85@gmail.com>
2015-09-28 11:20:20 -0700

    Added production settings file.

--- hellowebapp/wsgi.py
+++ hellowebapp/wsgi.py
@@ -1,5 +1,5 @@
 import os 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hellowebapp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hellowebapp.settings_production")
 from django.core.wsgi import get_wsgi_application 
 from whitenoise.django import DjangoWhiteNoise
 
